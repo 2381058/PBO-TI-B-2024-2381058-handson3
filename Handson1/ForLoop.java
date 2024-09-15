4b9c310d715b70a45269444779251f86a9103bbd
User <2381058@unai.edu>
2024-09-15 20:10:54 +0700

    Handson 3

---
+++ Handson1/ForLoop.java
@@ -0,0 +1,13 @@
+package Handson1;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        String[] buah = {"Apel", "Pisang", "Mangga", "Jeruk", "Durian", "Anggur"};
+        for (int i = 0; i < buah.length; i++) {
+            if (i == 3){
+                break;
+            }
+            System.out.println("Elemen ke " + (i + 1) + "adalah : " + buah[i]);
+        }
+    }
+}
---
+++ Handson1/WhileLoop.java
@@ -0,0 +1,12 @@
+package Handson1;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        String[] kumpulanBuah = {"Apel", "Pisang", "Mangga", "Jeruk", "Duiran", "Anggur"};
+        int counter = 0;
+        while (counter < kumpulanBuah.length){
+            System.out.println("Elemen ke " + (counter + 1) + "Adalah : " + kumpulanBuah[counter]);
+            counter++;
+        }
+    }
+}
